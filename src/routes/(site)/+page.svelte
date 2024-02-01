e1bf23abf9cd352cc52b7d4c7b9280673917d0b5
Tengku Ahmad Firdaus B Tengku Kamaruddin <firdaus.kamaruddin@sitecore.com>
2024-02-01 09:07:59 +0800

    update index page

--- src/routes/(site)/+page.svelte
+++ src/routes/(site)/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<div class="flex flex-col gap-2 items-center justify-center h-screen bg-app-light">
+	<h1 class="text-center">Hi, I am Firdaus</h1>
+</div>
