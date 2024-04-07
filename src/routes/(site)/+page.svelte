288289a674fa81139a0f1a9d97994b35c1966153
Tengku Ahmad Firdaus B Tengku Kamaruddin <firdaus.kamaruddin@sitecore.com>
2024-04-07 09:17:17 +0800

    update site

--- src/routes/(site)/+page.svelte
+++ src/routes/(site)/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 </script>
 
-<div class="flex flex-col gap-2 items-center justify-center h-screen bg-app-light">
-	<h1 class="text-center">Hi, I am Firdaus</h1>
+<div class="flex flex-col gap-2 items-center justify-center h-screen p-2 text-primary-700">
+	<h1 class="text-center text-5xl">Hi, I'm Firdaus</h1>
+	<p class="text-center text-2xl">I'm a software developer who writes about life, code, design, and more.</p>
+	<p class="text-center text-2xl">I am mainly focused on solution/application architectures & security for web-based (API) applications</p>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 const colors = require('tailwindcss/colors')
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content:  [
+    './src/**/*.{html,js,svelte,ts}',
+    './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+  ],
   theme: {
     extend: {
-      typography: ({theme}) => ({
-        DEFAULT: {
-          css: {
-            maxWidth: '2000px',
-          },
-        },
-      }),
       colors: {
         transparent: 'transparent',
         current: 'currentColor',
@@ -22,16 +18,17 @@ export default {
         yellow: colors.amber,
         pink: colors.fuchsia,
         primary: {
-          50: '#FFF5F2',
-          100: '#FFF1EE',
-          200: '#FFE4DE',
-          300: '#FFD5CC',
-          400: '#FFBCAD',
-          500: '#FE795D',
-          600: '#EF562F',
-          700: '#EB4F27',
-          800: '#CC4522',
-          900: '#A5371B'
+          50: '#ECFEFF',
+          100: '#CFFAFE',
+          200: '#A5F3FC',
+          300: '#67E8F9',
+          400: '#22D3EE',
+          500: '#06B6D4',
+          600: '#0891B2',
+          700: '#0E7490',
+          800: '#155E75',
+          900: '#164E63',
+          950: '#083344',
         },
         app: {
           DEFAULT: "#64748B",
